fac1c9a184f01c63a0086a9e80d8a89c3728bb8c
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-01-10 17:27:22 +0530

    add file via git.

---
+++ Frame .py
@@ -0,0 +1,37 @@
+"""----------------------- < PYTHON TKINTER FRAME > -------------------------"""
+
+
+'''-----> Frame_Regions '''
+ 
+# from tkinter import *
+# root=Tk()
+# root.geometry('1350x1100')
+
+# root.title('Model Dimensions.')
+
+# f4=Frame(root,bg='skyblue',borderwidth=5,relief=SUNKEN)
+# f4.pack(side=TOP,fill='x')
+
+# f1=Frame(root,bg='blue',borderwidth=5,relief=SUNKEN)
+# f1.pack(side=LEFT,fill='y')
+
+# f2=Frame(root,bg='yellow',borderwidth=5,relief=SUNKEN)
+# f2.pack(anchor='e',side=LEFT,fill='y')
+
+# f3=Frame(root,bg='green',borderwidth=5,relief=SUNKEN)
+# f3.pack(side=RIGHT,fill='y')
+
+# l4=Label(f4,text='Frame_Region.',fg='black',bg='skyblue',
+#  font=('Helvetica',20,'bold')).pack()
+
+# l1=Label(f1,text='\t\t\t.',fg='black',bg='blue',
+#  font=('Helvetica',20,'bold')).pack()
+
+# l2=Label(f2,text='''\t\t\t\t\t
+#          ''',fg='black',bg='yellow',
+#  font=('Helvetica',20,'bold')).pack(fill='x')
+
+# l3=Label(f3,text='\t\t\t',fg='black',bg='green',
+#  font=('Helvetica',20,'bold')).pack()
+
+# root.mainloop() 
